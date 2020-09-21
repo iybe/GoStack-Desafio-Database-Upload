fa913bf58f94bf346488431e654d0484931cd156
iybe <iesleybezerra2@gmail.com>
2020-09-20 23:08:32 -0300

    subindo

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,20 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+import TransactioRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionsRepository = getCustomRepository(TransactioRepository);
+
+    const transaction = await transactionsRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('A transaction nao existe');
+    }
+
+    await transactionsRepository.remove(transaction);
   }
 }
 
